6ea9263ac9b239b3097c781efef7b1f1773c09ca
Aakeefah Jones <aakeefahj@gmail.com>
2023-11-24 14:46:26 +0200

    almost done

---
+++ script/code.js
@@ -0,0 +1,11 @@
+const btnSubmit =
+document.getElementById('btnSubmit')
+btnSubmit.addEventListener('click', minutesToSeconds)
+
+let input = document.getElementById('input1');
+
+function minutesToSeconds(){
+    let minutes = document.querySelector('[data-input]');
+    let seconds = document.querySelector('[data-output]')
+}
+
