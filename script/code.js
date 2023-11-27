7bf212c4256e9184568b66282f44596028478072
Aakeefah Jones <aakeefahj@gmail.com>
2023-11-27 09:08:17 +0200

    done

--- script/code.js
+++ script/code.js
@@ -1,6 +1,6 @@
 document.getElementById('btnSubmit').addEventListener('click', function(){
 
     let input = document.querySelector('[data-input]').value;
-    let seconds = input * 60; document.querySelector('[data-output]').textContent = seconds;
+    let seconds = input * 60; document.querySelector('[data-output]').textContent = `${seconds.toFixed(2)}seconds`;
 
 });
